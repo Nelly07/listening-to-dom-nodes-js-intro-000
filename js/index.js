07d6386beb3982812241d4d937ba30550d5e66a1
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-28 21:43:38 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,3 +19,11 @@ input.addEventListener('keydown', function(e){
         console.log(e.key)
       }
     });
+
+let divs=document.querySelectorAll('div');
+function bubble(e){
+  console.log(this.firstChild.nodeValue.trim()+'bubbled');
+  for (let i =0; i < divis.lenght; i++){
+    divs[i].addEventListener('click',bubble);
+  }
+}
